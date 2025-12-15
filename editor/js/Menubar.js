198d926ba028eac24df4563e92d6b8239d79cb9a
julianne-pixel <julianne@stiegleredtech.org>
2025-12-15 15:18:39 -0500

    Update Menubar.js

--- editor/js/Menubar.js
+++ editor/js/Menubar.js
@@ -3,7 +3,9 @@ import { UIPanel } from './libs/ui.js';
 import { MenubarEdit } from './Menubar.Edit.js';
 import { MenubarFile } from './Menubar.File.js';
 import { MenubarHelp } from './Menubar.Help.js';
+/*
 import { MenubarStatus } from './Menubar.Status.js';
+*/
 
 function Menubar( editor ) {
 
@@ -13,8 +15,9 @@ function Menubar( editor ) {
 	container.add( new MenubarFile( editor ) );
 	container.add( new MenubarEdit( editor ) );
 	container.add( new MenubarHelp( editor ) );
-
+/*
 	container.add( new MenubarStatus( editor ) );
+	*/
 
 	return container;
 
