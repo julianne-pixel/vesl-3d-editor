d84e1ed86d57797e62a637885fcfd0a9f52d8c9b
julianne-pixel <julianne@stiegleredtech.org>
2025-12-15 12:06:55 -0500

    Update Sidebar.js

--- editor/js/Sidebar.js
+++ editor/js/Sidebar.js
@@ -1,16 +1,48 @@
 import { SidebarAddShapes } from './Sidebar.AddShapes.js';
-import { UITabbedPanel } from './libs/ui.js';
+
+import { UITabbedPanel, UISpan } from './libs/ui.js';
+
+import { SidebarScene } from './Sidebar.Scene.js';
+import { SidebarProperties } from './Sidebar.Properties.js';
+import { SidebarProject } from './Sidebar.Project.js';
+import { SidebarSettings } from './Sidebar.Settings.js';
 
 function Sidebar( editor ) {
 
-  const container = new UITabbedPanel();
-  container.setId( 'sidebar' );
+	const strings = editor.strings;
 
-  const addShapes = new SidebarAddShapes( editor );
-  container.addTab( 'shapes', 'Shapes', addShapes );
-  container.select( 'shapes' );
+	// THIS is the real sidebar container
+	const container = new UITabbedPanel();
+	container.setId( 'sidebar' );
 
-  return container;
+	// --- ADD YOUR PANEL HERE ---
+	const addShapes = new SidebarAddShapes( editor );
+	container.addTab( 'shapes', 'Shapes', addShapes );
+
+	const sidebarProperties = new SidebarProperties( editor );
+
+	const scene = new UISpan().add(
+		new SidebarScene( editor ),
+		sidebarProperties
+	);
+	const project = new SidebarProject( editor );
+	const settings = new SidebarSettings( editor );
+
+	container.addTab( 'scene', strings.getKey( 'sidebar/scene' ), scene );
+	container.addTab( 'project', strings.getKey( 'sidebar/project' ), project );
+	container.addTab( 'settings', strings.getKey( 'sidebar/settings' ), settings );
+	container.select( 'scene' );
+
+	const sidebarPropertiesResizeObserver = new ResizeObserver( function () {
+
+		sidebarProperties.tabsDiv.setWidth( getComputedStyle( container.dom ).width );
+
+	} );
+
+	sidebarPropertiesResizeObserver.observe( container.tabsDiv.dom );
+
+	return container;
+
 }
 
 export { Sidebar };
